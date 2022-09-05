3afbcace26e688fb1270feac745c91fb9effd0ac
Patryk <patryk.bogdanski95@gmail.com>
2022-09-05 23:41:27 +0200

    Add styles for product card

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -1,21 +1,32 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { ProductDTO } from "../../app/models/productDTO";
 
 export default function ProductCard({ product }: Props)
 {
     return (
         <Card>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: 'secondary.main' }}>
+                        {product.name.charAt(0).toUpperCase()}
+                    </Avatar>
+                }
+                title={product.name}
+                titleTypographyProps={{
+                    sx: { fontWeight: 'bold', color: 'primary.main' }
+                }}
+                subheader={`${product.brand} / ${product.category}`}
+
+            />
             <CardMedia
+                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
                 component="img"
                 height="140"
                 image={product.pictureUrl}
                 alt="product"
             />
             <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {product.name}
-                </Typography>
-                <Typography gutterBottom variant="h6" component="div">
+                <Typography gutterBottom color='secondary' variant="h5">
                     {product.price} zł
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
@@ -23,8 +34,8 @@ export default function ProductCard({ product }: Props)
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Kup teraz</Button>
-                <Button size="small">Obserwuj</Button>
+                <Button size="small">Add to cart</Button>
+                <Button size="small">View</Button>
             </CardActions>
         </Card>
     )
